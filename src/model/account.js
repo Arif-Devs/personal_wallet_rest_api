ec7131f51aeae5c5cdd1663945a72dd3e8e019b3
Arifur Rahman <arifur.sew@gmail.com>
2024-08-19 21:28:44 +0600

    account

--- src/model/account.js
+++ src/model/account.js
@@ -1,12 +1,27 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const accountSchema = new Schema({
-  name: {
-    require: true,
-    type: String,
+const accountSchema = new Schema(
+  {
+    name: {
+      require: true,
+      type: String,
+    },
+    accountDetails: {
+      require: true,
+      type: String,
+    },
+    initialValue: {
+      require: true,
+      type: Number,
+    },
+    userId: {
+      require: true,
+      type: mongoose.type.objectId,
+      ref: 'User',
+    },
   },
-  accountDetails: {
-    require: true,
-    type: String,
-  },
-});
+  { timestamps: true }
+);
+
+const Account = model('Account', accountSchema);
+export default Account;
